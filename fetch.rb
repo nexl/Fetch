ac9edad15144712089a32c2dd1e2e195ae454c04
steven <steven.jun@hotmail.com>
2023-11-22 02:23:30 +0900

    Create a function to get HTML file metadata

--- fetch.rb
+++ fetch.rb
@@ -12,4 +12,19 @@ def fetch_websites(*urls)
   end
 end
 
-fetch_websites(*ARGV[0..-1])
+def get_metadata(file_path)
+  if File.exist?(file_path)
+    html_file = File.read(file_path)
+    total_links = html_file.scan(/<a/).length
+    total_images = html_file.scan(/<img/).length
+    modification_time = File.mtime(file_path)
+    formatted_date = modification_time.strftime("%a %b %d %Y %H:%M %Z")
+    puts "Last modified date of #{file_path}: #{formatted_date}"
+    puts "site: #{file_path}"
+    puts "num_links: #{total_links}"
+    puts "images: #{total_images}"
+    puts "last fetch: #{formatted_date}"
+  else 
+    puts "File not found"
+  end
+end
