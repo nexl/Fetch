3288ab8780164af304650f38c06d014f77941004
steven <steven.jun@hotmail.com>
2023-11-22 02:08:48 +0900

    Update `fetch_websites` to receive URLs as many as the user wants

--- fetch.rb
+++ fetch.rb
@@ -1,13 +1,15 @@
 require 'open-uri'
 
-def fetch_website(url)
-  begin
-    website = URI.open(url).read
-    filename = "#{URI.parse(url).host}.html"
-    File.open(filename, 'w') { |file| file.write(website) }
-  rescue => exception
-    puts "Something went wrong, error message: #{exception.message}"
+def fetch_websites(*urls)
+  urls.each do | url | 
+    begin
+      website = URI.open(url).read
+      filename = "#{URI.parse(url).host}.html"
+      File.open(filename, 'w') { |file| file.write(website) }
+    rescue => exception
+      puts "Something went wrong, error message: #{exception.message}"
+    end
   end
 end
 
-fetch_website(ARGV[0])
+fetch_websites(*ARGV[0..-1])
